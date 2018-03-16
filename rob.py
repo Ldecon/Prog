f90e0b9238273280d6a3035f07d630dbc8727401
Ldecon <36233076+Ldecon@users.noreply.github.com>
2018-03-16 16:56:31 +0100

    Update rob.py

--- rob.py
+++ rob.py
@@ -18,7 +18,7 @@ class Robot:
 	
 	def newactualpos(self,p):
 		self.actualpos=p
-		self.actualpos.cont=self.actualpos.cont+1
+		self.actualpos.passcount=self.actualpos.passcount+1
 		self.possiblepos=self.actualpos.adj
 
 
@@ -28,25 +28,13 @@ class Robot:
 			print(self.possiblepos[x].pos,end=' ')
 		print(end='\n')
 
-	def resetcont(self,g):
+	def resetcounts(self,g):
 		for x in range(len(g.nodes)):
-			g.nodes[x].cont=0
+			g.nodes[x].passcount=0
+			g.nodes[x].visitcount=0
 		
-	############# scelta random  ########
-	def randomnextstep(self):
-		p=self.possiblepos[randint(0,len(self.possiblepos)-1)]
-		self.newactualpos(p)
-		
-	def randomsteps(self,ns,g):    #numstep
-		self.resetcont(g)
-		self.actualpos.cont=self.actualpos.cont+1
-		for x in range(ns):
-			#print('step ',x+1)
-			#print('robot in:',self.actualpos.pos)
-			#self.printpossiblepos()
-			self.randomnextstep()
 			
-	########## 
+	########## dijkstra  ########################################################################################
 	
 	def listnodesdij(self,ln):
 		l=[]
@@ -134,16 +122,49 @@ class Robot:
 		s=self.minpathnodetonode(n2,d,s)
 		s.reverse()
 		return s
-
 	
-	############ scelta tra importanza #######
+	
+	
+	############# scelta random  ########################################################################
+	
+	def updatevaluesn(self,n):
+		n.passcount=n.passcount+1
+		
+	
+	def updatevcount(self,n,t):
+		n.visitcount=n.visitcount+1
+		n.lastvisit=t
+	
+	def randomsteps(self,ns,g):    #numstep
+		self.resetcounts(g)
+		self.updatevaluesn(self.actualpos)
+		self.updatevcount(self.actualpos,1)
+		x=0
+		while x <= ns:
+			#print('step ',x+1)
+			#print('robot in:',self.actualpos.pos)
+			#self.printpossiblepos()
+			next=g.nodes[randint(0,len(g.nodes)-1)]
+			d=self.dijkstra(self.actualpos,next,g)
+			d.pop(0)
+			for y in range(len(d)):
+				x=x+1
+				if x >= ns:
+					break
+				r.actualpos=d[y].n
+				self.updatevaluesn(self.actualpos)
+				if d[y].n.pos == next.pos:
+					x=x+1
+					if x >= ns:
+						break
+					self.updatevcount(self.actualpos,x+1)
+	
+	############ scelta tra importanza ######################################################################
 	
 	def nodeidleness(self,n,t):			#node, time
 		return abs(t-n.lastvisit)
 		
-	def updatevaluesn(self,n,t):
-		n.cont=n.cont+1
-		n.lastvisit=t
+
 	
 	
 	def nextstep(self,n,t,g):			#node, time, graph
@@ -157,8 +178,9 @@ class Robot:
 				
 				
 	def imppath(self,ns,g):      #numstep,graph
-		self.resetcont(g)
-		self.updatevaluesn(self.actualpos,1)
+		self.resetcounts(g)
+		self.updatevaluesn(self.actualpos)
+		self.updatevcount(self.actualpos,1)
 		x=0
 		#print('Step', x+1)
 		#print('Robot in:', self.actualpos.pos)
@@ -173,16 +195,14 @@ class Robot:
 				r.actualpos=d[y].n
 				#print('Step', x+1)
 				#print('Robot in:', self.actualpos.pos)
-				self.updatevaluesn(self.actualpos,x+1)
-	
-	def maxcont(self,t):
-		n=0
-		for x in range(len(t)):
-			if t[x].cont >= n:
-				n=t[x].cont
-				a=t[x]
-		return a
-	
+				self.updatevaluesn(self.actualpos)
+				if d[y].n.pos == next.pos:
+					x=x+1
+					if x >= ns:
+						break
+					self.updatevcount(self.actualpos,x+1)
+					
+					
 	def listnamepos(self,ln):
 		auspos=[]
 		for x in range(len(ln)):
@@ -193,33 +213,30 @@ class Robot:
 		return auspos
 				
 	def stats(self,ns,g):
-		#temp=g.nodes.copy()
 		aus=g.nodes.copy()
-		#while temp:
-		#	aus.append(self.maxcont(temp))
-		#	for x in range(len(temp)):
-		#		if  temp[x].pos == aus[len(aus)-1].pos:
-		#			temp.pop(x)
-		#			break
-		ls=range(len(aus))
-		auspos=[]
-		auscont=[]	
+		ausvcount=[]
+		auspcount=[]	
+		print('Pass')
 		for x in range(len(aus)):
-			auscont.append(aus[x].cont)
+			auspcount.append(aus[x].passcount)
 			if aus[x].imp==4:
-				auspos.append('CR '+aus[x].pos)
 				print('crnode -> ',end='')
-			else:
-				auspos.append(aus[x].pos)
-			print('Node:',aus[x].pos,'imp=',aus[x].imp,'visits:',aus[x].cont, 'times',(aus[x].cont/ns)*100,'%')
-		return auscont
+			print('Node:',aus[x].pos,'imp=',aus[x].imp,'visits:',aus[x].passcount, 'times',(aus[x].passcount/ns)*100,'%')
+		print('Visits')
+		for x in range(len(aus)):
+			ausvcount.append(aus[x].visitcount)
+			if aus[x].imp==4:
+				print('crnode -> ',end='')
+			print('Node:',aus[x].pos,'imp=',aus[x].imp,'visits:',aus[x].visitcount, 'times',(aus[x].visitcount/ns)*100,'%')
+		return auspcount,ausvcount
 	
-	def plotbar(self,d,x,y):
+	
+	def plotbarpass(self,d,x,y):
 		ls=range(len(x))
-		plt.figure('Stats', figsize=(20, 6))
+		plt.figure('Stats', figsize=(20, 12))
 		plt.title('Nodes visits\' stats')
 		for z in range(len(y)):
-			plt.subplot(1,len(y),z+1)
+			plt.subplot(2,len(y)/2,z+1)
 			plt.bar(ls,y[z], color='g')	
 			plt.title(d[z])
 			plt.xticks(ls,x,rotation='vertical')
@@ -229,7 +246,7 @@ class Robot:
 		plt.show()	
 		
 		
-		##################### imp norm
+		##################### imp norm ############################################################################
 		
 	def maxidleness(self,t,g):
 		maxidl=0.0
@@ -257,8 +274,9 @@ class Robot:
 		return next
 			
 	def impnormpath(self,ns,g):
-		self.resetcont(g)
-		self.updatevaluesn(self.actualpos,1)
+		self.resetcounts(g)
+		self.updatevaluesn(self.actualpos)
+		self.updatevcount(self.actualpos,1)
 		x=0
 		#print('Step', x+1)
 		#print('Robot in:', self.actualpos.pos)
@@ -273,8 +291,12 @@ class Robot:
 				r.actualpos=d[y].n
 				#print('Step', x+1)
 				#print('Robot in:', self.actualpos.pos)
-				self.updatevaluesn(self.actualpos,x+1)
-		
+				self.updatevaluesn(self.actualpos)
+				if d[y].n.pos == next.pos:
+					x=x+1
+					if x >= ns:
+						break
+					self.updatevcount(self.actualpos,x+1)
 		
 	
 
@@ -286,19 +308,25 @@ r=Robot(n)
 env.g.printg()
 env.g.printedges()
 y=[]
-d=['Random','imp*idleness','(imp/impmax)*(idleness/idlenessmax)']
+d=['Random Pass','imp*idleness Pass','(imp/impmax)*(idleness/idlenessmax) Pass','Random Visits','imp*idleness Visits','(imp/impmax)*(idleness/idlenessmax) Visits' ]
 print('Mode: random')
 r.randomsteps(500,env.g)
-y.append(r.stats(500,env.g))
+y1,y2=r.stats(500,env.g)
 print()
 #d=r.dijkstra(n,n1,env.g)
 print('Mode: imp*idleness')
 r.imppath(1000,env.g)
-y.append(r.stats(1000,env.g))
+y3,y4=r.stats(1000,env.g)
 print()
 print('Mode: (imp/impmax)*(idleness/idlenessmax)')
 r.impnormpath(1000,env.g)
-y.append(r.stats(1000,env.g))
+y5,y6=r.stats(1000,env.g)
 print()
-r.plotbar(d,r.listnamepos(env.g.nodes),y)
+y.append(y1)
+y.append(y3)
+y.append(y5)
+y.append(y2)
+y.append(y4)
+y.append(y6)
+r.plotbarpass(d,r.listnamepos(env.g.nodes),y)
 
