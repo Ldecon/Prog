9095d35ff599c57b696fbf2ae8af21412d5eaf7a
Ldecon <36233076+Ldecon@users.noreply.github.com>
2018-05-09 12:05:52 +0200

    Update Env.py

--- Env.py
+++ Env.py
@@ -1,10 +1,11 @@
 from random import random, randint
-
+import math
 
 class Node:
-	def __init__(self,pos,imp=-1,cover=0):
+	def __init__(self,pos,imp=-1):
 		self.pos=pos
-		self.cover=cover
+		self.cx=-1
+		self.cy=-1
 		self.adj=[]
 		self.imp=imp
 		self.visitcount=0
@@ -19,70 +20,75 @@ class Node:
 	def remadj(self,n):
 		if n in self.adj:
 			self.adj.remove(n)
-	
-	def setcover(self,c):
-		self.cover=c
-	
+		
 	def listadj(self):		
 		return self.adj[:]
-
-	def __eq__(self,n):
-		return n.pos==self.pos
-
-	def __ne__(self,n):
-		return not(n==self)
-	
-	def __hash__(self):
-        	return self.pos.__hash__()
+		
+	def __del__(self):
+		del self 	
 
 
 class Edge:
-	def __init__(self,n1,n2,w=0):
+	def __init__(self,n1,n2):
 		self.n1=n1
 		self.n2=n2
-		self.w=w
-	
-
+		self.w=math.sqrt(((n1.cx-n2.cx)**2)+((n1.cy-n2.cy)**2))	
+		
 class Graph:
-	def __init__(self,numnodes=0):
+	def __init__(self,numnodes=0, planex=100, planey=100):   		#plane=grandezza piano cartesiano default 100x100
 		self.numnodes=numnodes
 		self.nodes=[]
 		self.edges=[]
 		self.matnodes={}
+		self.planex=planex
+		self.planey=planey
+		self.dispcoord=[]
+		for i in range(self.planex):
+			for j in range(self.planey):
+				self.dispcoord.append([i,j])
 		
 	def getnode(self,n):
-		return self.nodes[self.matnodes[n]]
-		
-	def getweightedge(self,n1,n2):
-		for x in range(len(self.edges)):
-				if ((self.edges[x].n1.pos == n1.pos) and (self.edges[x].n2.pos == n2.pos))or((self.edges[x].n1.pos == n2.pos) and (self.edges[x].n2.pos == n1.pos)):
-					return self.edges[x].w
-		return 0
-
-	def existedge(self,n1,n2):
-		for x in range(len(self.edges)):
-			if ((self.edges[x].n1.pos == n1.pos) and (self.edges[x].n2.pos == n2.pos))or((self.edges[x].n1.pos == n2.pos) and (self.edges[x].n2.pos == n1.pos)):
-				return 1
-		return 0
-		
+		for i in range(len(self.nodes)):
+			if n.pos==self.nodes[i].pos :
+				return self.nodes[i]
+		return None
+	
 	def getedge(self,n1,n2):
 		for x in range(len(self.edges)):
 				if ((self.edges[x].n1.pos == n1.pos) and (self.edges[x].n2.pos == n2.pos))or((self.edges[x].n1.pos == n2.pos) and (self.edges[x].n2.pos == n1.pos)):
 					return self.edges[x]
-		return None		
-							
+		return None
+	
+	
+	def getweightedge(self,n1,n2):
+		e=self.getedge(n1,n2)
+		if e:
+			return self.edges[x].w
+		return 0
+			
+			
+	def existedge(self,n1,n2):
+		e=self.getedge(n1,n2)
+		if e:
+			return 1
+		return 0	
+			
 	def addnode(self,n):
 		if n not in self.matnodes:
+			r=randint(0,len(self.dispcoord)-1)
+			n.cx=self.dispcoord[r][0]
+			n.cy=self.dispcoord[r][1]
+			self.dispcoord.pop(r)
 			self.nodes.append(n)
 			self.matnodes[n]=len(self.nodes)-1
 			self.numnodes=self.numnodes+1
-		return self.getnode(n)
+		return 
 	
 	def remnode(self,n):
 		self.nodes.remove(n)
-		#self.matnodes.remove(n)
 		self.numnodes=self.numnodes-1
-
+		return
+	
 	def addedge(self,n,m):
 		n1= self.getnode(n)
 		m1=self.getnode(m)
@@ -91,6 +97,7 @@ class Graph:
 		if not self.existedge(n,m):
 			e=Edge(n,m)
 			self.edges.append(e)	
+		return
 			
 	def remedge(self,e):
 		n1=self.getnode(e.n1)
@@ -99,24 +106,11 @@ class Graph:
 		n2.remadj(self.getnode(e.n1))
 		if self.existedge(n1,n2):
 			self.edges.remove(self.getedge(self.getnode(n1),self.getnode(n2)))
-	
+		return
+		
 	def setimpn(self,n,i):
 		n.imp=i
-				
-	
-	def setweightedge(self,n1,n2,w):
-		for x in range(len(self.edges)):
-			if ((self.edges[x].n1.pos == n1.pos) and (self.edges[x].n2.pos == n2.pos))or((self.edges[x].n1.pos == n2.pos) and (self.edges[x].n2.pos == n1.pos)):
-				self.edges[x].w=w
-			
-	def trianglepath(self,n1,n2):					#scopre se c'è un percorso triangolare
-		t=[]
-		for x in range(len(n1.adj)):
-			for y in range(len(n2.adj)):
-				if (n1.adj[x].pos==n2.adj[y].pos)and(n1.adj[x] not in t):
-					t.append(n1.adj[x])
-		return t
-	
+		return
 	
 	def critnodes(self,ln,num):			#scelta nodi critici
 		lc=[]
@@ -139,19 +133,6 @@ class Graph:
 				d=d-1
 		return lc
 		
-	
-		
-	#def cascadeimp(self,n,i,imax):			#nodo, importanza corrente, importanza massima	
-	#	imp=i-(imax/(len(self.nodes))*i)
-	#	if (n.imp != -1) and (n.imp > imp):
-	#		return
-	#	else:
-	#		self.setimpn(n,imp)
-	#		for x in range(len(n.adj)):
-	#			self.cascadeimp(n.adj[x],imp,imax)
-		
-		
-		#importanza set dimezzamento
 	def cascadeimp(self,n,i):			#nodo, importanza corrente  
 		imp=i/2
 		if (n.imp != -1)and(n.imp > imp):
@@ -182,11 +163,9 @@ class Graph:
 			return
 		for a in self.matnodes:
 			x=self.getnode(a)
-			print(x.pos, 'i=', x.imp, ":", end="")
+			print(x.pos, ", coord=(",x.cx,",",x.cy,") i=", x.imp, ":", end="")
 			for n in x.adj:
 				print(" ", n.pos,  end="")
-				if n.cover:
-					print('()', end="")
 			print()
 			
 	def printgfile(self):
@@ -194,14 +173,16 @@ class Graph:
 		for a in self.matnodes:
 			x=self.getnode(a)
 			f.write(x.pos)
-			f.write(' i=')
+			f.write(', coord=(')
+			f.write(str(x.cx))
+			f.write(',')
+			f.write(str(x.cy))
+			f.write(') i=')
 			f.write(str(x.imp))
 			f.write(':')
 			for n in x.adj:
 				f.write(' ')
 				f.write(n.pos)
-				if n.cover:
-					f.write('()')
 			f.write('\n')
 		for x in range(len(self.edges)):
 			f.write('[')
@@ -220,89 +201,87 @@ class Graph:
 		while len(self.nodes) :
 			del self.matnodes[self.nodes[0]]
 			self.remnode(self.nodes[0])
-		del self
+		
 			
 	def __del__(self):
-		del self
+		del self	
+			
 
 class Environment:
-	def __init__(self,n=0,file=0,g=Graph(),ed=-1):
+	def __init__(self,n=0,file=0,g=Graph(),ed=-1):                          #n=numero nodi, file=no/sì caricamento da file, g=grafo,ed=coefficiente percentuale archi
 		self.g=g
 		self.n=n
 		self.ed=ed
-		aux=[]
 		self.file=file
 		if not file:
 			for x in range(n):
-				aux.append(Node(str(x+1)))  #popolamento lista ausiliaria
-			c=randint(0,n-1)
-			self.g.addnode(aux[c])
-			aux.pop(c)
-			while aux:							#popolamento lista nodi grafo e svuotamento lista ausiliaria
-				r=randint(0,len(self.g.nodes)-1)
-				c=randint(0, len(aux)-1)
-				self.g.addnode(aux[c])
-				ndaux=self.g.getnode(aux[c])
-				self.g.addedge(self.g.nodes[r],ndaux)
-				w=randint(1,10)						#pesi possibili degli archi
-				self.g.setweightedge(self.g.nodes[r],ndaux,w)      #all'arco attuale viene dato il peso w
-				aux.pop(c)
-			if(self.ed<0):
-				ra=randint(0,int((len(self.g.nodes)*len(self.g.nodes)-1)/2))
+				g.addnode(Node(str(x+1)))				#inserimento nodi nell'ambiente
+				for i in range(len(g.nodes)-1):
+					g.addedge(g.nodes[len(g.nodes)-1],g.nodes[i])
+			if self.ed<0 :
+				ran=randint(0,((self.g.numnodes*(self.g.numnodes-1))/2)-(self.g.numnodes-1))      #numero casuale archi da 0 a (n*n-1/2)-n-1 (per preservare la componenete connessa)
 			else:
-				ra=int(((len(self.g.nodes)*len(self.g.nodes)-1)/2)*self.ed)
-			re=[]	
-			for x in range(ra):						#aggiunta un numero di archi casuale tra 0 e n(n-1)/2
-				c=randint(0,len(self.g.nodes)-1)
-				r=randint(0,len(self.g.nodes)-1)
-				if (c != r) and(not self.g.existedge(self.g.nodes[c],self.g.nodes[r])):
-					self.g.addedge(self.g.nodes[c],self.g.nodes[r])
-					e=self.g.edges[len(self.g.edges)-1]
-					t=self.g.trianglepath(e.n1,e.n2)
-					if t:
-						listab=[]
-						lists=[]
-						for y in range(len(t)):
-							e1=self.g.getedge(e.n1,t[y])
-							e2=self.g.getedge(t[y],e.n2)
-							s=e1.w+e2.w
-							ab=abs(e1.w-e2.w)
-							listab.append(ab)
-							listab.sort()
-							lists.append(s)
-							lists.sort()
-							#print(e.n1.pos,'-',e.n2.pos,'#############################',e1.n1.pos,'-',e1.n2.pos,'(',e1.w,')','->',e2.n1.pos,'-',e2.n2.pos,'(',e2.w,')',s)	
-						if (lists[0] >= listab[0])and(listab[len(listab)-1]<=lists[0]):			#se min(somma) >= min(differenza) & max(differenza) <= min(somma)
-							if listab[len(listab)-1]==0:
-								listab[len(listab)-1]=1
-							ranw=randint(listab[len(listab)-1],lists[0])
-							e.w=ranw
-						else:
-							re.append(e)	
-					else:
-						ranw=randint(1,10)
-						e.w=ranw
-						
-				for y in range(len(re)):
-					self.g.remedge(self.g.getedge(re[y].n1,re[y].n2))
-				re=[]		
+				ran=int((((self.g.numnodes*(self.g.numnodes-1))/2)-(self.g.numnodes-1)) * (1-self.ed))      #si tolgono gli archi in percentuale dato da ed
 			
+			aux=[]
+			edgetree=[]
+			edgeposs=[]
+			ln=g.nodes.copy()
+			r1=randint(0,len(ln)-1)
+			aux.append(ln[r1])
+			ln.pop(r1)
+			while ln:									#crea sottoalbero di connessioni
+				ss=[]
+				for i in range(len(aux)):
+					if len(aux)==1:
+						r1=1
+					else:	
+						r1=randint(0,len(ln)-1)	
+					for j in range(r1):
+						r2=randint(0,len(ln)-1)
+						ss.append(ln[r2])
+						edgetree.append(g.getedge(aux[i],ln[r2]))
+						ln.pop(r2)
+				if len(ln)==1:
+					edgetree.append(g.getedge(edgetree[len(edgetree)-1].n1,ln[0]))
+					ln.pop(0)
+				aux=ss.copy()
+			edgeposs=[i for i in g.edges if i not in edgetree]
+			
+			
+			for i in range(ran):							#eliminazione archi
+				r1=randint(0,len(edgeposs)-1)
+				g.remedge(edgeposs[r1])
+				edgeposs.pop(r1)
+				
 			self.g.setimpg()
-			
-			
-			
+		
 		else:
 			self.file=open('Graph.txt')
 			r=self.file.readline()
 			while r[0]!='[':					#da file inserimento nodi
 				i=0
 				s=''
-				while r[i]!=' ':
+				while r[i]!=',':
 					s=s+r[i]
 					i=i+1
 				n=Node(s)
 				self.g.addnode(n)
 				s=''
+				while r[i]!='(':
+					i=i+1
+				i=i+1
+				while r[i]!=',':
+					s=s+r[i]
+					i=i+1
+				n.cx=int(s)
+				i=i+1
+				s=''	
+				while r[i]!=')':
+					s=s+r[i]
+					i=i+1
+				n.cy=int(s)
+				s=''	
 				while r[i]!='=':
 					i=i+1
 				i=i+1
@@ -311,7 +290,7 @@ class Environment:
 					i=i+1
 				n.imp=float(s)
 				r=self.file.readline()
-					
+			
 			while r!='':					#da file inserimento archi
 				i=1	
 				s=''
@@ -325,26 +304,10 @@ class Environment:
 					i=i+1
 				n1=Node(s)
 				n2=Node(s1)
-				for x in range(len(self.g.nodes)):
-					if n1.pos == self.g.nodes[x].pos:
-						n1=self.g.getnode(self.g.nodes[x])
-						break
-				for x in range(len(self.g.nodes)):	
-					if n2.pos == self.g.nodes[x].pos:
-						n2=self.g.getnode(self.g.nodes[x])
-						break
-				self.g.addedge(n1,n2)
-				e=self.g.getedge(n1,n2)
-				while r[i]!='=':
-					i=i+1
-				i=i+1
-				sw=''
-				while r[i]!=']':
-					sw=sw+r[i]
-					i=i+1
-				e.w=int(sw)
+				print(self.g.getnode(n1).pos,',',self.g.getnode(n2).pos)
+				self.g.addedge(self.g.getnode(n1),self.g.getnode(n2))
+
 				r=self.file.readline()
-		
 		
 	def destroye(self):
 		self.g.destroyg()
@@ -353,12 +316,7 @@ class Environment:
 				
 	def __del__(self):
 		del self
-	
+			
+			
 
-				
-		#self.g.printg()
-		#self.g.printgfile()
-		#self.g.printedges()
-		
-#e=Environment(10)				#creazione grafo da numero di nodi
-#e=Environment(file=1)			#lettura grafo da file
+
